b8c5a2359e7fd07917223eb283d89de6904af3d2
Ray Villalobos <ray@lynda.com>
2016-10-27 21:31:02 -0400

    02_01e Handlebars

--- process/js/app.js
+++ process/js/app.js
@@ -4,7 +4,26 @@ var Handlebars = require('handlebars');
 
 $(function() {
   var topoffset = 50;
-  $('.loader').fadeOut(1000);
+
+  $.getJSON('/data/pets.json', function(data) {
+    var slideshowTemplate = $('#slideshow-template').html();
+    var slideshowScript = Handlebars.compile(slideshowTemplate);
+
+    $('.loader').fadeOut(1000);
+    $('#slideshow-content').append(slideshowScript(data));
+
+    //replace IMG inside carousels with a background image
+    $('#slideshow .item img').each(function() {
+      var imgSrc = $(this).attr('src');
+      $(this).parent().css({'background-image': 'url(' + imgSrc + ')'});
+      $(this).remove();
+    });
+
+    //Activate carousel
+    $('.carousel').carousel({
+      pause: false
+    });
+  });
 
   $('.navbar-fixed-top').on('activate.bs.scrollspy', function() {
     var hash = $(this).find('li.active a').attr('href');
